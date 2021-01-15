b08c2f8a6707df78b0dee1573995c7d694d0501b
Nnarere <marevaantonella@gmail.com>
2021-01-15 10:48:01 -0300

    incorporación del json y otros arreglos menores al html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-class NuevosProductos () {
+/*class NuevosProductos () {
     constructor() {}
-    }
+    }*/
 
 function ProductConstructor(id, name, description, price) {
     this.id = id;
@@ -8,4 +8,6 @@ function ProductConstructor(id, name, description, price) {
     this.description= description;
     this.price = price;
 }
-var tartaDeFresas = new productConstructor(1, tarta de fresas, 3000);
+var tartaDeFresas = new ProductConstructor(1, "tarta de fresas", "rica tarta", 3000);
+
+console.log(ProductConstructor)
