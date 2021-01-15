6dfef4345ee6e66bfc3f12426771b1019e52231f
Nnarere <marevaantonella@gmail.com>
2021-01-15 09:34:17 -0300

    carga masiva inicial

---
+++ js/main.js
@@ -0,0 +1,11 @@
+class NuevosProductos () {
+    constructor() {}
+    }
+
+function ProductConstructor(id, name, description, price) {
+    this.id = id;
+    this.name = name;
+    this.description= description;
+    this.price = price;
+}
+var tartaDeFresas = new productConstructor(1, tarta de fresas, 3000);
