10e0385b42a6b4628b6576bc9ff35c55a21e4988
Nnarere <marevaantonella@gmail.com>
2021-01-20 18:46:19 -0300

    incorpore IF

--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
+const domBuilder = new buildProductCard()
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
     const productContainerPasteleria = document.getElementsByClassName("pasteleria__row")
 
     pasteleria.forEach(function(product){
-        const cardPasteleria = buildProductCard(pasteleria);
-        productContainerPasteleria.innerHTML += card;
+        if(product.avaliable){
+            const cardPasteleria = buildProductCard(pasteleria);
+            productContainerPasteleria.innerHTML += card;
+        }
   });
 
   const productContainerPanaderia = document.getElementsByClassName("panaderia__row")
 
     panaderia.forEach(function(product){
+        if(product.avaliable){
         const cardPanaderia = buildProductCard(panaderia);
             productContainerPanaderia.innerHTML += card;
+        }
   });
 
   const productContainerCafeteria = document.getElementsByClassName("cafeteria__row")
 
   cafeteria.forEach(function(product){
+    if(product.avaliable){
       const cardCafeteria = buildProductCard(pasteleria);
       productContainerCafeteria.innerHTML += card;
+    }
 });
 
 })
